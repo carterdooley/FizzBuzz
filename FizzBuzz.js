cc5f12d7ec0fc180aca70e1260a36f73351b2096
carter dooley <carter.j.dooley43@gmail.com>
2021-10-14 09:33:35 -0500

    Finished lab

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,1 +1,10 @@
 // Code
+for (let i = 1; i <= 51; i++) {
+    if (i % 3 === 0) {
+        console.log("fizz")
+    } else if (i % 5 === 0) {
+        console.log("buzz")
+    } else if (i > 0) {
+        console.log(i)
+    }
+}
